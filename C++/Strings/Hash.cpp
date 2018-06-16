6e51651034912914f26e44ec653ea67dbbbf21ff
DarkDream99 <denys.zaluzhnyi@gmail.com>
2018-06-16 22:40:55 +0300

    Add hash-function

--- C++/Strings/Hash.cpp
+++ C++/Strings/Hash.cpp
@@ -1,83 +1,39 @@
+/*
+ * Hash function for strings in ANSII incoding
+ */
+
 #include <iostream>
 #include <string>
-#include <vector>
 
 using std::string;
-using std::vector;
 
-struct Palindrom {
-	size_t from;
-	size_t size;
+unsigned long HashString(string str) {
+    unsigned long hash = 5381;
+    char symbol;
 
-	Palindrom(size_t from, size_t size) {
-		this->from = from;
-		this->size = size;
-	}
-};
+    for (int ind = 0; ind < str.size(); ++ind) {
+        symbol = str[ind];
+        hash = ((hash << 5) + hash) + symbol;
+    }
 
-struct Node {
-	size_t palindromSize;
-	vector<Node*> childs;
-	Node* suffixLink;
-
-	Node() {
-		palindromSize = 0;
-		suffixLink = nullptr;
-	}
-
-	Node(size_t palindromSize) {
-		this->palindromSize = palindromSize;
-		suffixLink = nullptr;
-	}
-};
-
-struct PalindromTree {
-	Node* evenHead;
-	Node* oddHead;
-	Node* tail;
-
-	PalindromTree() {
-		evenHead = new Node();
-		oddHead = new Node();
-
-		evenHead->suffixLink = evenHead;
-		
-		oddHead->palindromSize = 1;
-		oddHead->suffixLink = evenHead;
-
-		tail = oddHead;
-	}
-
-	vector<Palindrom*> SearchPalindroms(const string& text) {
-		vector<Palindrom> palindroms;
-		palindroms.push_back(Palindrom(0, 1));
-
-		for (int ind = 1; ind < text.size(); ++ind) {
-			char symbol = text[ind];
-
-			while (true) {
-				if (text[ind - tail->palindromSize] == symbol) {
-					size_t newPalindromSize = tail->palindromSize + 2;
-					palindroms.push_back(Palindrom(ind, newPalindromSize));
-					tail->childs.push_back(new Node(newPalindromSize));
-
-					break;
-				}
-
-				if (tail == evenHead) {
-
-
-					break;
-				}
-
-				tail = tail->suffixLink;
-			}
-		}
-	}
-};
+    return hash;
+}
 
 int main() {
+    string a = "Hello";
+    string b = "hello";
+    string c = "ello";
+    string d = "Hello";
 
-	
-	return 0;
+    unsigned long hashA = HashString(a);
+    unsigned long hashB = HashString(b);
+    unsigned long hashC = HashString(c);
+    unsigned long hashD = HashString(d);
+
+    std::cout << "Hash " << a << ": " << hashA << std::endl;
+    std::cout << "Hash " << b << ": " << hashB << std::endl;
+    std::cout << "Hash " << c << ": " << hashC << std::endl;
+    std::cout << "Hash " << d << ": " << hashD << std::endl;
+
+    return 0;
 }
