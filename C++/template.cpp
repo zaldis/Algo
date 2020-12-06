19ec5c73be88b759ead2a381b83b977f6d661fc2
Denys Zaluzhnyi <denys.zaluzhnyi@gmail.com>
2020-12-06 21:27:11 +0200

    Add cpp template for competitive programming

---
+++ C++/template.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i, a, b) for(int i=a; i<=b; ++i)
+
+typedef long long LL;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    return 0;
+}
