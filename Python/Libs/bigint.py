2203c391450af8f73c2ed0421ca0724384d2face
Denys Zaluzhnyi <denys.zaluzhnyi@gmail.com>
2021-05-09 19:36:24 +0300

    Add BigInteger representation

--- Python/Libs/bigint.py
+++ Python/Libs/bigint.py
@@ -2,6 +2,9 @@
     BigInteger is a data structure that emulates long arithmetic
 """
 
+from typing import Union
+
+
 class BigInteger:
     """
         maximum is 1e4000; 4 digits by block
@@ -13,28 +16,45 @@ class BigInteger:
     BLOCK_BASE = 10 ** BLOCK_SIZE
 
     def __init__(self, number: str):
-        self._digits = [0] * self.BLOCKS_COUNT
+        self._blocks = [0] * self.BLOCKS_COUNT
         self._used_size = 0
 
         for char in number:
             digit = int(char)
             # shift one digit:
+            # 145321234
             # [1234, 4532, 1] -> [1234, 4532, 10] -> [1234, 5320, 14] -> [2340, 5321, 14]
             for i in range(self._used_size, -1, -1):
-                self._digits[i+1] = self._digits[i+1] + (self._digits[i] * 10) // self.BLOCK_BASE
-                self._digits[i] = (self._digits[i] * 10) % self.BLOCK_BASE
-            self._digits[0] += digit
-            if self._digits[self._used_size + 1] != 0:
+                self._blocks[i+1] = self._blocks[i+1] + (self._blocks[i] * 10) // self.BLOCK_BASE
+                self._blocks[i] = (self._blocks[i] * 10) % self.BLOCK_BASE
+            self._blocks[0] += digit
+            if self._blocks[self._used_size + 1] != 0:
                 self._used_size += 1
 
     @classmethod
-    def create_from_number(cls, number: int):
-        return cls(str(number))
+    def create(cls, number: Union[int, str]):
+        if isinstance(number, int):
+            number = str(number)
+        return cls(number)
+
+    def __str__(self):
+        converted = list(
+            map(
+                lambda block: str(block).zfill(self.BLOCK_SIZE),
+                reversed(self._blocks[:self._used_size+1])
+            )
+        )
+        return ''.join(converted).lstrip('0')
+
 
 
 if __name__ == '__main__':
-    big_num = BigInteger('12143424123467182634612354757')
-    print(big_num._digits)
+    # Initialize from string representation
+    big_num = BigInteger.create('12143424123467182634612354757')
+    print(big_num._blocks)
+    print(big_num)
 
-    big_num = BigInteger.create_from_number(1267578512346)
-    print(big_num._digits)
+    # Initialize from number
+    big_num = BigInteger.create(1267578512346)
+    print(big_num._blocks)
+    print(big_num)
